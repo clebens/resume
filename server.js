f030871db37fdacb142fe56587b4d9b5f5d19760
Colin Lebens <colin.lebens@gmail.com>
2015-02-11 15:08:06 -0800

    Added skeleton API server and public index.

--- server.js
+++ server.js
@@ -1,7 +1,75 @@
 var express = require('express');
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
 
 var dblogin = fs.readFileSync("prv/dbpw", { "encoding" : "utf-8" } ).split(',');
-var dbu = dblogin[0];
-var dbp = dblogin[1];
+var dbu = dblogin[0].trim();
+var dbp = dblogin[1].trim();
+
+var dbdomain = 'ds041841.mongolab.com';
+var dbport = '41841';
+var dburl = 'mongodb://' + dbu + ':' + dbp + '@' + dbdomain + ':' + dbport + '/resume';
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/users/:user', function(req, res, next) {
+
+    var user = req.params.user;
+    
+    mongo.connect(dburl, function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    db.collection('users').findOne({username: user}, function(err, doc) {
+        console.log("Found the following records");
+        console.log(doc);
+        res.send(doc);
+        db.close();
+        });
+    });
+});
+app.get('/users/:user/jobs', function(req, res, next) {
+
+    var user = req.params.user;
+    
+    mongo.connect(dburl, function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    db.collection('users').findOne({username: user}, function(err, doc) {
+        console.log("Found the following records");
+        console.log(doc.jobs);
+        db.collection('jobs').find({'_id': { '$in' : doc.jobs }}).toArray(function(err, doc) {
+            console.log(err);
+            console.log(doc);
+            res.send(doc);
+            db.close();
+        });
+        });
+    });
+});
+
+app.get('/users/:user/education', function(req, res, next) {
+
+    var user = req.params.user;
+    
+    mongo.connect(dburl, function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    db.collection('users').findOne({username: user}, function(err, doc) {
+        console.log("Found the following records");
+        console.log(doc.education);
+        db.collection('education').find({'_id': { '$in' : doc.education }}).toArray(function(err, doc) {
+            console.log(err);
+            console.log(doc);
+            res.send(doc);
+            db.close();
+        });
+        });
+    });
+});
+
+app.listen(3000, function() {});
